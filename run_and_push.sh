9b5f22c22ef41afce1ba345fe10330de60a6324a
Your Name <you@example.com>
2025-07-29 09:31:08 -0400

    Update vehicle data and add script

---
+++ run_and_push.sh
@@ -0,0 +1,18 @@
+rm -f /home/dstnxtwo/car-data-json/car-data-json/.git/index.lock
+#!/bin/bash
+
+cd ~/car-data-json/car-data-json/
+
+# Pull latest changes from GitHub
+git pull origin main
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run the scraping script
+python3 test.py
+
+# Add and commit changes
+git add *.json
+git commit -m "Update JSON data on $(date)"
+git push origin main
